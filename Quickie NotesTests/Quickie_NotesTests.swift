80659187fb115a74a55eb488e43202b4c2dc7d8e
phillyphil2004 <philipparkinson@Philips-Macbook-Air.local>
2015-02-14 16:26:03 -0500

    Initial Commit

---
+++ Quickie NotesTests/Quickie_NotesTests.swift
@@ -0,0 +1,36 @@
+//
+//  Quickie_NotesTests.swift
+//  Quickie NotesTests
+//
+//  Created by Philip Parkinson on 2/14/15.
+//  Copyright (c) 2015 Young. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Quickie_NotesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
